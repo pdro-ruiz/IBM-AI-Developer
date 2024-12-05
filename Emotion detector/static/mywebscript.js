232c3d88fdd8c5eaf5d31ad6269711a327ffc0cd
Pedro Ruiz <32985142+pdro-ruiz@users.noreply.github.com>
2024-12-05 11:24:04 +0100

    Update mywebscript.js

--- Emotion detector/static/mywebscript.js
+++ Emotion detector/static/mywebscript.js
@@ -1,12 +1,20 @@
-let RunSentimentAnalysis = ()=>{
-    textToAnalyze = document.getElementById("textToAnalyze").value;
+let RunSentimentAnalysis = () => {
+    const textToAnalyze = document.getElementById("textToAnalyze").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("system_response").innerHTML = xhttp.responseText;
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                const response = JSON.parse(xhttp.responseText);
+                document.getElementById("system_response").innerHTML = `
+                    <pre>${JSON.stringify(response, null, 2)}</pre>`;
+            } else {
+                document.getElementById("system_response").innerHTML = `
+                    <p style="color: red;">Error: ¡Texto inválido! ¡Por favor, inténtalo de nuevo!</p>`;
+            }
         }
     };
-    xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
+
+    xhttp.open("GET", `/emotionDetector?textToAnalyze=${encodeURIComponent(textToAnalyze)}`, true);
     xhttp.send();
-}
+};
