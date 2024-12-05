7f9be38a4086728d736247f438067111daa6671c
Pedro Ruiz <32985142+pdro-ruiz@users.noreply.github.com>
2024-12-05 11:14:34 +0100

    Update emotion_detection.py

--- Emotion detector/emotion_detection.py
+++ Emotion detector/emotion_detection.py
@@ -1,1 +1,51 @@
+import requests
+import json
 
+# Función para detectar emociones
+def emotion_detector(text_to_analyze):
+    """
+    Detecta las emociones de un texto dado utilizando Watson NLP.
+
+    Args:
+        text_to_analyze (str): Texto a analizar.
+
+    Returns:
+        dict: Diccionario con las emociones y la emoción dominante.
+    """
+    if not text_to_analyze.strip():
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
+        }
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
+        }
+
+    emotions = response.json()['emotion_predictions']
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': emotions.get('anger', 0),
+        'disgust': emotions.get('disgust', 0),
+        'fear': emotions.get('fear', 0),
+        'joy': emotions.get('joy', 0),
+        'sadness': emotions.get('sadness', 0),
+        'dominant_emotion': dominant_emotion,
+    }
